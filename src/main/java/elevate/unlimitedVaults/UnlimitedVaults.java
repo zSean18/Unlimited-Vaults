a4f8aa3c727eeb3aaf0a864c629a280c8047c6d9
zSean18 <s.m.rachwalski@gmail.com>
2025-09-24 17:17:46 -0700

    Initial Commit

---
+++ src/main/java/elevate/unlimitedVaults/UnlimitedVaults.java
@@ -0,0 +1,17 @@
+package elevate.unlimitedVaults;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UnlimitedVaults extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
